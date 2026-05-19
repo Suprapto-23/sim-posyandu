9d4a66eb1c6ac82e1f4824fcccc8e6c72eb30d62
Suprapto <ssuprapto351@gmail.com>
2026-05-19 16:02:02 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,4 @@
-﻿<?php
-
+<?php
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
-        // 1. Trust semua proxy (wajib untuk Vercel agar CSRF/session bisa dibaca)
         $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
             \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
             \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
@@ -20,17 +18,19 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
         );
 
-        // 2. Alias middleware
+        // Fix 419 - exclude CSRF untuk login
+        $middleware->validateCsrfTokens(except: [
+            'login',
+        ]);
+
         $middleware->alias([
             'role'        => \App\Http\Middleware\RoleMiddleware::class,
             'checkstatus' => \App\Http\Middleware\CheckUserStatus::class,
             'logactivity' => \App\Http\Middleware\LogUserActivity::class,
         ]);
 
-        // 3. Redirect guest ke login
         $middleware->redirectGuestsTo('/login');
 
-        // 4. Redirect user yang sudah login ke dashboard sesuai role
         $middleware->redirectUsersTo(function () {
             $user = auth()->user();
             if (!$user) return '/login';
